e01f2646b443ec36ef8572c8427f4589220d003c
Adalid Josue Sarmiento <sarmiento.josue97@gmail.com>
2026-04-16 00:24:36 -0400

    disparos asteroides

--- controlador.js
+++ controlador.js
@@ -34,12 +34,46 @@ class Controlador_Juego{
     }
 
     _actualizar_Logica(){
+
         this.nave.actualizar_Logica(this.teclas);
+
         this.disparos.forEach(d => d.actualizar_Logica());
         this.asteroides.forEach(a => a.actualizar_Logica());
-
         this.disparos = this.disparos.filter(d => d.esta_vivo());
+
+        this._detectar_Colisiones();
     }
+
+
+    _detectar_Colisiones() {
+        for (let i = this.asteroides.length - 1; i >= 0; i--) {
+            const ast = this.asteroides[i];
+ 
+            for (let j = this.disparos.length - 1; j >= 0; j--) {
+                const dis = this.disparos[j];
+ 
+                const distancia = Math.hypot(dis.x - ast.x, dis.y - ast.y);
+ 
+                if (distancia < dis.radio + ast.radio) {
+                    this.disparos.splice(j, 1); 
+                    this.asteroides.splice(i, 1);
+ 
+                    this.puntos += 100;
+ 
+                    if (this.asteroides.length === 0) {
+                        for (let k = 0; k < 7; k++) {
+                            this.asteroides.push(
+                                new Modelo_Asteroide(this.lienzo.width, this.lienzo.height)
+                            );
+                        }
+                    }
+ 
+                    break;
+                }
+            }
+        }
+    }
+
 
     _actualizar_Vista(){
         this.vista.dibujar_Escena(this.nave, this.asteroides, this.disparos);
